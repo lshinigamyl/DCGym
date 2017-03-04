43b89054614fcad498bea05d31d3dae2f62ce24d
romyangie <1330571@utp.edu.pe>
2017-03-04 01:24:39 -0500

    Modificando y añadiendo metodos get y set a Routine

--- src/pe/com/dcgym/models/Routine.java
+++ src/pe/com/dcgym/models/Routine.java
@@ -3,79 +3,52 @@ package pe.com.dcgym.models;
 /**
  * Created by Fjorsvartnir on 23/02/2017.
  */
- public class Routine {
-     private int id;
+public class Routine {
+    private int id;
     private String name;
     private String description;
     private String state;
-	public  Routine()	{
-	}
-		public  Routine(int id, String name,  String description, String state){
-		
-		this.id = id;
-        this.name = name;
-        this.description = description;
-        this.state = state;
-   
-		}
-		
-	
-    /**
-     * @return the id
-     */
+
+    public Routine(int id, String name, String description, String state) {
+        this.setId(id);
+        this.setName(name);
+        this.setDescription(description);
+        this.setState(state);
+    }
+    public Routine(){
+
+    }
+
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
 
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the description
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description the description to set
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return the state
-     */
     public String getState() {
         return state;
     }
 
-    /**
-     * @param state the state to set
-     */
     public void setState(String state) {
         this.state = state;
     }
+}
 
-
-
- }
