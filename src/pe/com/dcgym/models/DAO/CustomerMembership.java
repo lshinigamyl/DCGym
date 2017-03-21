2467cd31ea53abbcd36bf131c9f4291bbb21964d
romyangie <1330571@utp.edu.pe>
2017-03-20 19:03:28 -0500

    eliminando codigo basura.

--- src/pe/com/dcgym/models/DAO/CustomerMembership.java
+++ src/pe/com/dcgym/models/DAO/CustomerMembership.java
@@ -6,16 +6,7 @@ import pe.com.dcgym.models.DTO.MembershipsTypeEntity;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/*
-Las clases en Java son básicamente una plantilla que
-sirve para crear un objeto. Si imaginásemos las clases
-en el mundo en el que vivimos, podríamos decir que la
-clase “persona” es una plantilla sobre cómo debe ser un ser
-humano. Todos y cada uno de nosotros, los seres humanos, somos
-objetos de la clase “persona“, ya que todos somos personas. La clase
-“persona” contiene la definición de un ser humano, mientras que cada
-ser humano es una instancia u objeto de dicha clase.
- */
+
 
 public class CustomerMembership {
     private int id;
