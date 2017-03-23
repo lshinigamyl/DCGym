90cb25b10362a6ad43deecd8d0bf2efafc718463
Haydee Esthefany <1533422@utp.edu.pe>
2017-03-23 01:02:11 -0500

    Modificando LOGIN para que redireccione a su respectivo MENU segun TIPO

--- src/pe/com/dcgym/services/ClientService.java
+++ src/pe/com/dcgym/services/ClientService.java
@@ -1,6 +1,8 @@
 package pe.com.dcgym.services;
 
+import pe.com.dcgym.models.DAO.Customer;
 import pe.com.dcgym.models.DAO.People;
+import pe.com.dcgym.models.DAO.TrainingCenter;
 
 import java.util.List;
 
@@ -12,4 +14,5 @@ public class ClientService extends BaseService {
     public People findPeopleById(String user){
         return getPeopleEntity().findByUser(user);
     }
+
 }
