f65240ecc8896bc3f18cb8b70f2e86bd665bc483
Carolai22 <okamiwolf21@outlook.com>
2017-03-25 04:19:19 -0500

    Arreglando codigo

--- src/pe/com/dcgym/models/DAO/Employee.java
+++ src/pe/com/dcgym/models/DAO/Employee.java
@@ -7,9 +7,100 @@ import pe.com.dcgym.models.DTO.TrainingCentersEntity;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
+/*
+public abstract class Guerrero {
+    private String nombre;
+    private int edad;
+    
+    private int fuerza;
+    private boolean herido;
 
- */
+    private boolean muerto;
+
+    public Guerrero(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+boolean comprobarEdad(int e){
+boolean ed;
+ if (e>=15 && e<=60){
+ ed = true;
+}
+        return false;
+}
+
+boolean comprobarFuerza(int f){
+boolean fu;
+ if (f>=1 && f<=10){
+ fu = true;
+}
+        return false;
+}
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Guerrero(String nombre, int edad, int fuerza) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.fuerza = fuerza;
+    }
+   
+
+    public Guerrero() {
+       this("Guerrero x",15,1);
+    }
+
+    public Guerrero(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean retirarse(){
+        
+        return false;
+    
+    }
+
+    */
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public boolean isHerido() {
+        return herido;
+    }
+
+    public void setHerido(boolean herido) {
+        this.herido = herido;
+    }
+
+    public boolean isMuerto() {
+        return muerto;
+    }
+
+    public void setMuerto(boolean muerto) {
+        this.muerto = muerto;
+    }
+    
+   
+
+
 public class Employee {
     int id;
     String state;
